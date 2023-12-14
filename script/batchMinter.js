c0010c8c4f8c30b8ce9fee6c2c31b57dec6c79b2
Jackson <2045330878@qq.com>
2023-12-14 20:04:30 +0800

    Update batchMinter.js

--- script/batchMinter.js
+++ script/batchMinter.js
@@ -1,16 +1,15 @@
 const { ethers } = require('ethers');
 
-const providerUrl = 'https://rpc.fantom.network';
+const providerUrl = 'RPC节点';
 const provider = new ethers.providers.JsonRpcProvider(providerUrl);
 const privateKey = '私钥';
 const wallet = new ethers.Wallet(privateKey, provider);
-const recipientAddress = '转移的地址（自转）'; 
+const targetAddress = '转移的目的地址'; 
 
 function sleep(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
-// 获取当前账户的 nonce
 async function getCurrentNonce(wallet) {
     try {
       const nonce = await wallet.getTransactionCount("pending");
@@ -21,31 +20,29 @@ async function getCurrentNonce(wallet) {
       throw error;
     }
 }
-//开打
+
 async function sendData() {
-    const data = '0x646174613a2c7b2270223a226672632d3230222c226f70223a226d696e74222c227469636b223a2266616e73222c22616d74223a223130303030227d';
+    const data = '16进制数据';
 
-    let nonce = await getCurrentNonce(wallet);
-    let amount = 1000;   //循环次数
-    for (let i = 0; i < amount; i++) {
+    let nonce = 0;
+    let currentGasPrice = 0;
+    let amount = 1000    // 循环次数
+    for (let i = 0; i < amount; i++) {    
         try {
-            let j = i % 10;
+            let j = i % 10;    //  每10次交易重新获取nonce和GasPrice
             if(j == 0) {
-                sleep(5000);
                 nonce = await getCurrentNonce(wallet);
+                currentGasPrice = await provider.getGasPrice();
             }
-
-            let currentGasPrice = await provider.getGasPrice();
             const tx = await wallet.sendTransaction({
-                to: recipientAddress,
-                value: 0, 
+                to: targetAddress,
+                value: 0,
                 data: data,
                 gasPrice: currentGasPrice,
-                gasLimit: 25000,
-                chainId: 250,
                 nonce: nonce + j
             });
-            console.log(`第 ${nonce + j} 次数据交易哈希:`, tx.hash);
+            await sleep(300);    //暂停0.3秒，可根据实际情况修改
+            console.log(`第 ${i + 1} 次数据交易哈希:`, tx.hash);
         } catch (error) {
             i--;
             console.error('发生异常:', error.message);
